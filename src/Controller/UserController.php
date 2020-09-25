c0e1685cafe4796e3e5fb06b1eaf238caadf1897
fabien hamayon <fabienhamayon@free.fr>
2020-09-26 00:16:22 +0200

    create user with role

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -33,6 +33,8 @@ class UserController extends AbstractController
 
         $form->handleRequest($request);
 
+
+
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $password = $encoder->encodePassword($user, $user->getPassword());
