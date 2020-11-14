65bbfba9bc589eaca25d3af2886239ae8a4ff231
fabien hamayon <fabienhamayon@free.fr>
2020-11-14 17:53:23 +0100

    install encore

--- src/.preload.php
+++ src/.preload.php
@@ -1,5 +1,5 @@
 <?php
 
-if (file_exists(__DIR__.'/../var/cache/env/App_KernelEnvDebugContainer.preload.php')) {
-    require __DIR__.'/../var/cache/env/App_KernelEnvDebugContainer.preload.php';
+if (file_exists(__DIR__.'/../var/cache/dev/App_KernelDevDebugContainer.preload.php')) {
+    require __DIR__.'/../var/cache/dev/App_KernelDevDebugContainer.preload.php';
 }
